a9ce46652e0e3df70fa69115552a363ff1514131
Sunil Ballani <ballanisunil123@gmail.com>
2021-06-15 19:56:17 +0530

    Express sessions part 2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,9 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose  = require('mongoose');
 const Dishes = require('./models/dishes')
+const Promotions = require('./models/promotions');
+const Leaders = require('./models/leaders');
+const { signedCookie } = require('cookie-parser');
 const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url);
 
@@ -28,11 +33,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:'false',
+  resave:false,
+  store:new FileStore()
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.session);
+
+  if(!req.session.user){
+    var err = new Error('You are not authenticated');
+    err.status = 401;
+    return next(err);
+  }
+
+  else{
+    if(req.session.user === 'authenticated'){
+      next();
+  }     
+    else{
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      next(err);
+    }            
+  }
+  }
+  
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
