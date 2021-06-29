a953cdbd1067a8fed093079bf411232e2548f12b
Sunil Ballani <ballanisunil123@gmail.com>
2021-06-29 17:05:18 +0530

    Mongoose population

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -17,7 +20,7 @@ const Dishes = require('./models/dishes')
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
 const { signedCookie } = require('cookie-parser');
-const url = 'mongodb://localhost:27017/conFusion'
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db)=>{
@@ -34,40 +37,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name:'session-id',
-  secret:'12345-67890-09876-54321',
-  saveUninitialized:'false',
-  resave:false,
-  store:new FileStore()
-}));
+app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.session);
-
-  if(!req.session.user){
-    var err = new Error('You are not authenticated');
-    err.status = 401;
-    return next(err);
-  }
-
-  else{
-    if(req.session.user === 'authenticated'){
-      next();
-  }     
-    else{
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }            
-  }
-  }
-  
-
-app.use(auth);
+app.use('/', index);
+app.use('/users', users);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
